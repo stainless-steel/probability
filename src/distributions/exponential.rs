0d9be3c125b7145d8a163ea4450083ecf513fca4
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-23 22:50:28 -0400

    Respect 99 characters

--- src/distributions/exponential.rs
+++ src/distributions/exponential.rs
@@ -2,17 +2,17 @@ use rand::distributions::{Exp, IndependentSample};
 
 use {Distribution, Generator};
 
-/// A continuous exponential distribution with the rate parameter
-/// `lambda`.
+/// A continuous exponential distribution with rate `lambda`.
 #[derive(Clone, Copy)]
 pub struct Exponential {
-    /// The rate parameter
+    /// The rate parameter.
     pub lambda: f64,
+
     sampler: Exp,
 }
 
 impl Exponential {
-    /// Create an exponential distribution with `rate = lambda`.
+    /// Create an exponential distribution with rate `lambda`.
     ///
     /// # Panics
     ///
@@ -41,8 +41,7 @@ impl Distribution for Exponential {
 
     #[inline]
     fn inv_cdf(&self, p: f64) -> f64 {
-        debug_assert!(0.0 <= p && p <= 1.0,
-                      "inv_cdf called with `p` not between 0 and 1");
+        debug_assert!(0.0 <= p && p <= 1.0, "inv_cdf called with `p` not between 0 and 1");
         -(-p).ln_1p() / self.lambda
     }
 
@@ -70,35 +69,30 @@ mod tests {
     #[test]
     fn pdf() {
         let exponential = Exponential::new(2.0);
-        let x = vec![
-            -1.0, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.0, 6.0, 12.0
-        ];
+        let x = vec![-1.0, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.0, 6.0, 12.0];
         let p = vec![
-            0.0, 2.0, 7.357588823428847e-1, 2.706705664732254e-1,
-            9.957413673572789e-2, 3.663127777746836e-2, 1.3475893998170934e-2,
-            4.957504353332717e-3, 6.709252558050237e-4,
-            1.228842470665642e-5, 7.550269088558195e-11
+            0.000000000000000e+00, 2.000000000000000e+00, 7.357588823428847e-01,
+            2.706705664732254e-01, 9.957413673572789e-02, 3.663127777746836e-02,
+            1.347589399817093e-02, 4.957504353332717e-03, 6.709252558050237e-04,
+            1.228842470665642e-05, 7.550269088558195e-11,
         ];
 
-        assert_eq!(&x.iter().map(|&x| exponential.pdf(x)).collect::<Vec<_>>(), &p);
+        assert::within(&x.iter().map(|&x| exponential.pdf(x)).collect::<Vec<_>>(), &p, 1e-15);
     }
 
     #[test]
     fn cdf() {
         let exponential = Exponential::new(2.0);
-        let x = vec![
-            -1.0, 0.0, 0.01, 0.05, 0.1, 0.15,
-            0.25, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0
-        ];
+        let x = vec![-1.0, 0.0, 0.01, 0.05, 0.1, 0.15, 0.25, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0];
         let p = vec![
-            0.0, 0.0, 1.98013266932447e-2, 9.516258196404043e-2,
-            0.18126924692201815, 0.2591817793182821, 0.3934693402873666,
-            0.6321205588285577, 0.8646647167633873, 0.950212931632136,
-            0.9816843611112658, 0.9975212478233336, 0.9996645373720975
+            0.000000000000000e+00, 0.000000000000000e+00, 1.980132669324470e-02,
+            9.516258196404043e-02, 1.812692469220182e-01, 2.591817793182821e-01,
+            3.934693402873666e-01, 6.321205588285577e-01, 8.646647167633873e-01,
+            9.502129316321360e-01, 9.816843611112658e-01, 9.975212478233336e-01,
+            9.996645373720975e-01
         ];
 
-        assert::within(&x.iter().map(|&x| exponential.cdf(x)).collect::<Vec<_>>(),
-                       &p, 1e-18);
+        assert::within(&x.iter().map(|&x| exponential.cdf(x)).collect::<Vec<_>>(), &p, 1e-15);
     }
 
     #[test]
@@ -107,18 +101,17 @@ mod tests {
 
         let exponential = Exponential::new(2.0);
         let x = vec![
-            0.0, 0.01, 0.05, 0.1, 0.15, 0.25, 0.5, 1.0, 1.5, 2.0,
-            3.0, 4.0, INFINITY
+            0.0, 0.01, 0.05, 0.1, 0.15, 0.25, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, INFINITY,
         ];
         let p = vec![
-            0.0, 1.98013266932447e-2, 9.516258196404043e-2,
-            0.18126924692201815, 0.2591817793182821, 0.3934693402873666,
-            0.6321205588285577, 0.8646647167633873, 0.950212931632136,
-            0.9816843611112658, 0.9975212478233336, 0.9996645373720975, 1.0
+            0.000000000000000e+00, 1.980132669324470e-02, 9.516258196404043e-02,
+            1.812692469220182e-01, 2.591817793182821e-01, 3.934693402873666e-01,
+            6.321205588285577e-01, 8.646647167633873e-01, 9.502129316321360e-01,
+            9.816843611112658e-01, 9.975212478233336e-01, 9.996645373720975e-01,
+            1.000000000000000e-00,
         ];
 
-        assert::within(&p.iter().map(|&p| exponential.inv_cdf(p))
-                                .collect::<Vec<_>>(), &x, 1e-14);
+        assert::within(&p.iter().map(|&p| exponential.inv_cdf(p)).collect::<Vec<_>>(), &x, 1e-14);
     }
 
     #[test]
