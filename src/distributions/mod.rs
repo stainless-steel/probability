b35a03e4e61c197ac72ab3b92f4a2b55b78eb505
runarberg <runarberg@zoho.com>
2015-06-10 03:24:40 +0000

    Implemented the categorical distribution

--- src/distributions/mod.rs
+++ src/distributions/mod.rs
@@ -1,12 +1,14 @@
 //! Probability distributions.
 
 pub use self::bernoulli::Bernoulli;
+pub use self::categorical::Categorical;
 pub use self::beta::Beta;
 pub use self::exponential::Exponential;
 pub use self::gaussian::Gaussian;
 pub use self::uniform::Uniform;
 
 mod bernoulli;
+mod categorical;
 mod beta;
 mod exponential;
 mod gaussian;
