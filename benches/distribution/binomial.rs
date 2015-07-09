44ebc529322921ee5e36af97e9f4fbab929d0458
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-09 15:34:24 -0400

    A cosmetic adjustment

--- benches/distribution/binomial.rs
+++ benches/distribution/binomial.rs
@@ -10,18 +10,18 @@ fn cdf(bencher: &mut Bencher) {
 }
 
 #[bench]
+fn pmf(bencher: &mut Bencher) {
+    let d = Binomial::new(100_000, 0.845);
+    let x = Independent(&d, &mut random::default()).take(1000).collect::<Vec<_>>();
+
+    bencher.iter(|| black_box(x.iter().map(|&x| d.pmf(x)).collect::<Vec<_>>()));
+}
+
+#[bench]
 fn inv_cdf(bencher: &mut Bencher) {
     let d = Binomial::new(100_000, 0.845);
     let p = Independent(&Uniform::new(0.0, 1.0), &mut random::default()).take(1000)
                                                                         .collect::<Vec<_>>();
 
     bencher.iter(|| black_box(p.iter().map(|&p| d.inv_cdf(p)).collect::<Vec<_>>()));
-}
-
-#[bench]
-fn pmf(bencher: &mut Bencher) {
-    let d = Binomial::new(100_000, 0.845);
-    let x = Independent(&d, &mut random::default()).take(1000).collect::<Vec<_>>();
-
-    bencher.iter(|| black_box(x.iter().map(|&x| d.pmf(x)).collect::<Vec<_>>()));
 }
