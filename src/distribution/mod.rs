41e1352763b3a115996149b7f4d33c68354fcb91
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-07-01 16:35:53 +0200

    Make a cosmetic adjustment

--- src/distribution/mod.rs
+++ src/distribution/mod.rs
@@ -93,15 +93,6 @@ pub trait Variance: Mean {
     }
 }
 
-pub use self::bernoulli::Bernoulli;
-pub use self::beta::Beta;
-pub use self::binomial::Binomial;
-pub use self::categorical::Categorical;
-pub use self::exponential::Exponential;
-pub use self::gamma::Gamma;
-pub use self::gaussian::Gaussian;
-pub use self::uniform::Uniform;
-
 mod bernoulli;
 mod beta;
 mod binomial;
@@ -110,3 +101,12 @@ mod exponential;
 mod gamma;
 mod gaussian;
 mod uniform;
+
+pub use self::bernoulli::Bernoulli;
+pub use self::beta::Beta;
+pub use self::binomial::Binomial;
+pub use self::categorical::Categorical;
+pub use self::exponential::Exponential;
+pub use self::gamma::Gamma;
+pub use self::gaussian::Gaussian;
+pub use self::uniform::Uniform;
