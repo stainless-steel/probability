5da2499cca4e20a512f5e6c515260adbadbc2969
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-10 21:49:32 +0100

    Run rustfmt-nightly in src/sampler/

--- src/sampler/mod.rs
+++ src/sampler/mod.rs
@@ -6,7 +6,11 @@ use source::Source;
 /// A means of drawing a sequence of independent samples.
 pub struct Independent<D, S>(pub D, pub S);
 
-impl<'a, T, D, S> Iterator for Independent<&'a D, &'a mut S> where D: Sample<Value=T>, S: Source {
+impl<'a, T, D, S> Iterator for Independent<&'a D, &'a mut S>
+where
+    D: Sample<Value = T>,
+    S: Source,
+{
     type Item = T;
 
     #[inline(always)]
