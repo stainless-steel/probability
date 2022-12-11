85f3cacd491bb5add7d98edb24ab8816087415b1
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-12-11 19:18:19 +0100

    Update the inverse of binomial

--- src/distribution/binomial.rs
+++ src/distribution/binomial.rs
@@ -216,7 +216,7 @@ impl distribution::Inverse for Binomial {
             loop {
                 let delta = alpha * (p - self.distribution(q)) / self.mass(q as usize);
                 if delta.abs() < 0.5 {
-                    return (q + delta) as usize;
+                    return q as usize;
                 }
                 q += delta;
                 alpha *= ALPHA;
